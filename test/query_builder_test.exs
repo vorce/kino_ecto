6e3d6d96aae180f417ef1d0bdefdea962d0fb72f
Joel C <joel@vorce.se>
2022-11-18 08:33:46 +0100

    Fix query_builder_test

--- test/query_builder_test.exs
+++ test/query_builder_test.exs
@@ -1,18 +1,16 @@
 defmodule KinoEcto.QueryBuilderTest do
   use ExUnit.Case
 
-  import Ecto.Query
-
   alias KinoEcto.QueryBuilder
 
   test "parses simple query" do
     query = "SELECT * FROM persons WHERE name = 'John'"
 
-    IO.inspect(QueryBuilder.call(query))
+    IO.inspect(QueryBuilder.call(%QueryBuilder{sql_query: query}))
     IO.inspect(QueryBuilder.test(query))
   end
 
   test "parses simple with where" do
-    query = "SELECT name FROM persons WHERE name = 'John'"
+    _query = "SELECT name FROM persons WHERE name = 'John'"
   end
 end
