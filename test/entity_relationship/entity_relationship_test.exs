ed8220960992acc3b11e18500eae05da87d57192
Matthew Boehlig <thetamind@gmail.com>
2022-11-20 18:30:39 -0600

    Escape class name on other side of relationship in ERD

--- test/entity_relationship/entity_relationship_test.exs
+++ test/entity_relationship/entity_relationship_test.exs
@@ -4,19 +4,49 @@ defmodule KinoEcto.EntityRelationshipTest do
 
   alias KinoEcto.EntityRelationship
 
-  defmodule TestTeam do
+  defmodule Organization do
     use Ecto.Schema
+    alias KinoEcto.EntityRelationshipTest.Team
+
+    schema "organizations" do
+      field(:name, :string)
+      has_one(:team, Team)
+    end
+  end
+
+  defmodule Team do
+    use Ecto.Schema
+    alias KinoEcto.EntityRelationshipTest.Person
 
     schema "teams" do
       field(:name, :string)
+      has_many(:persons, Person)
+    end
+  end
+
+  defmodule Person do
+    use Ecto.Schema
+    alias KinoEcto.EntityRelationshipTest.{Organization, Team}
+
+    schema "persons" do
+      field(:name, :string)
+      field(:age, :integer)
+
+      has_one(:team, Team)
+      belongs_to(:organization, Organization)
     end
   end
 
   test "render schema" do
-    assert {:markdown, md} = Kino.Render.to_livebook(%EntityRelationship{schema: TestTeam})
+    alias KinoEcto.EntityRelationshipTest.Person
+    assert {:markdown, md} = Kino.Render.to_livebook(%EntityRelationship{schema: Person})
 
     assert md =~ "mermaid"
-    assert md =~ "class `KinoEcto.EntityRelationshipTest.TestTeam`"
-    assert md =~ "`KinoEcto.EntityRelationshipTest.TestTeam` : string name"
+    assert md =~ "class `KinoEcto.EntityRelationshipTest.Team`"
+    assert md =~ "`KinoEcto.EntityRelationshipTest.Team` : string name"
+    assert md =~ "`KinoEcto.EntityRelationshipTest.Person` : integer age"
+
+    assert md =~
+             "`KinoEcto.EntityRelationshipTest.Organization` \"0\" --|> \"1\" `KinoEcto.EntityRelationshipTest.Team`"
   end
 end
